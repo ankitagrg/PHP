6a2c36f03b667cdda8bd80279f8f153f01d275a8
ankitagrg <ankitalg18@gmail.com>
2024-01-08 16:09:23 +0545

    php

--- register.php
+++ register.php
@@ -1,16 +1,14 @@
 <?php require '../connection/config.php'; ?>
 <!doctype html>
 <html lang="en">
-
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <title>Title</title>
 </head>
-
 <body>
-  
+   
     <div class="container col-6 my-5 py-5">
         <h1 class="text-center">Add User</h1>
         <?php
